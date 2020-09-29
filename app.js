0fccea5438b30cef765434113c4f77a4f4a70bc2
Joeway <joewaybanh@hotmail.com>
2020-09-29 17:17:03 +0200

    brainstorming ideas for logic

---
+++ app.js
@@ -0,0 +1,20 @@
+
+let cardSelection = [1, 1, 2, 2, 3, 3,];
+
+const compareCards = () =>
+if (cardSelection[i] == cardSelection[i]){
+    function(trueValue) {
+        Playedcards = 
+        else return startposition 
+    }
+}
+
+
+/-------------------------
+const computerChoise = (computerInput) => {
+    computerInput = Math.floor(Math.random(computerInput)*3)+1
+    randomCard = computerInput
+}
+
+const card = document.createElement("div");
+  card.classList.add("card");
